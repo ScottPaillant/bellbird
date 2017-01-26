9dad18f6e4dda99bb725ba54535cbdefdb31b04d
ScottPaillant <scottpaillant@gmail.com>
2017-01-26 18:32:16 +0000

    Automatically add dates, remove date selector

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,2 +1,6 @@
 class Alarm < ApplicationRecord
+  def text=(value)
+    write_attribute(:text, value.upcase)
+    self.date = Time.now.strftime("%d/%m/%Y %H:%M:%S")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :alarms
-  root 'application#hello'
+  root 'alarms#index'
 end
